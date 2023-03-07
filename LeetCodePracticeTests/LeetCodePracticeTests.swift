0bd8c8a694d99b87985144785a68e086d803804c
Will_Weng <alangprs1@gmail.com>
2023-03-07 17:11:27 +0800

    feat: 遞減方式算法

--- LeetCodePracticeTests/LeetCodePracticeTests.swift
+++ LeetCodePracticeTests/LeetCodePracticeTests.swift
@@ -10,4 +10,27 @@ import XCTest
 
 final class LeetCodePracticeTests: XCTestCase {
 
+    func arrangeCoins(_ n: Int) -> Int {
+        /// 行數
+        var row = 1
+        /// 每行硬幣數量
+        var rowCoin = n - 1
+
+        while rowCoin >= row + 1 {
+            row += 1
+            rowCoin -= row
+        }
+
+        if n == 0 {
+            return 0
+        } else {
+            return row
+        }
+    }
+
+    func test_tes() {
+        let answer = arrangeCoins(11)
+        print("will - answer: \(answer)")
+    }
+
 }
