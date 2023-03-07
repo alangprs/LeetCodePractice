8577c8441e083a076e01aad5b2139c8e5916ce72
Will_Weng <alangprs1@gmail.com>
2023-03-07 16:58:01 +0800

    fate: init

---
+++ LeetCodePracticeTests/LeetCodePracticeTests.swift
@@ -0,0 +1,13 @@
+//
+//  LeetCodePracticeTests.swift
+//  LeetCodePracticeTests
+//
+//  Created by cm0768 on 2023/3/7.
+//
+
+import XCTest
+@testable import LeetCodePractice
+
+final class LeetCodePracticeTests: XCTestCase {
+
+}
