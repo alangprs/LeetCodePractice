d919da7206ec9e96cb72d33ecc96b6c9cfd4dff0
Will_Weng <alangprs1@gmail.com>
2023-03-10 16:41:26 +0800

    feat: 107. Binary Tree Level Order Traversal II

--- LeetCodePracticeTests/LeetCodePracticeTests.swift
+++ LeetCodePracticeTests/LeetCodePracticeTests.swift
@@ -8,28 +8,66 @@
 import XCTest
 @testable import LeetCodePractice
 
+/// 樹結構
+public class TreeNode {
+    /// 資料
+    public var val: Int
+    /// 左邊指針位置
+    public var left: TreeNode?
+    /// 右邊指針位置
+    public var right: TreeNode?
+
+    init() { self.val = 0; self.left = nil; self.right = nil;}
+    init(val: Int, left: TreeNode? = nil, right: TreeNode? = nil, last: TreeNode? = nil) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+
 final class LeetCodePracticeTests: XCTestCase {
 
-    func arrangeCoins(_ n: Int) -> Int {
-        /// 行數
-        var row = 1
-        /// 每行硬幣數量
-        var rowCoin = n - 1
+    func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else { return [] }
 
-        while rowCoin >= row + 1 {
-            row += 1
-            rowCoin -= row
+        var result = [[Int]]()
+
+        /// 目前此層所有 node
+        var currentLevelNodes = [root]
+
+        // 執行條件：array 不是空的時候
+        while currentLevelNodes.isEmpty == false {
+
+            /// 每層內容
+            var currentLevelValues = [Int]()
+            /// 下一層 node
+            var nextLevelNodes = [TreeNode]()
+
+            for node in currentLevelNodes {
+                currentLevelValues.append(node.val)
+
+                if let leftNode = node.left {
+                    nextLevelNodes.append(leftNode)
+                }
+
+                if let rightNode = node.right {
+                    nextLevelNodes.append(rightNode)
+                }
+            }
+
+            result.insert(currentLevelValues, at: 0)
+            currentLevelNodes = nextLevelNodes
         }
 
-        if n == 0 {
-            return 0
-        } else {
-            return row
-        }
+        return result
     }
 
     func test_tes() {
-        let answer = arrangeCoins(11)
+
+        let tree = TreeNode(val: 3, left: TreeNode(val: 9, left: nil, right: nil), right: TreeNode(val: 20, left: TreeNode(val: 15, left: nil, right: nil), right: TreeNode(val: 7)))
+
+        let answer = levelOrderBottom(tree)
         print("will - answer: \(answer)")
     }
 
